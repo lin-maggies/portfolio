cf61a8a0860932fbc123f3e187a5571aa3989c36
maggie <lin.maggies@gmail.com>
2022-03-14 23:19:34 +1300

    half setup contact me form and backend

--- src/Containers/contactMe.js
+++ src/Containers/contactMe.js
@@ -3,14 +3,27 @@ import { Text, Button, Box, Link } from "@chakra-ui/react";
 import { useNavigate } from "react-router";
 import "../App.css";
 import Navbar from "../Components/Navbar";
+import router from "../routes/router.js";
 
 export default function ContactMe() {
   const navigate = useNavigate();
+
   return (
     <div>
       <Navbar />
       <h1>Contact me</h1>
       <p>blahhhhh</p>
+      <form action="" onSubmit={router}>
+        <div>
+          <p>Email</p>
+          <input type="text" name="emailTo" required />
+          <p>Name</p>
+          <input type="text" name="name" required />
+          <p>message</p>
+          <input type="text" name="message" required />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
