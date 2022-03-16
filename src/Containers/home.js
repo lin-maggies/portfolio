707d0ff4bb39671125098c41f791041c7f83482e
lin.maggies <lin.maggies@gmail.com>
2022-03-16 18:17:06 +1300

    changed some css and wording

--- src/Containers/home.js
+++ src/Containers/home.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Text, Button, Box } from "@chakra-ui/react";
-import { useNavigate } from "react-router";
-import { Link } from "react-router-dom";
-import Navbar from "../Components/Navbar";
-import "../App.css";
+import React, { useState, useEffect, useContext } from 'react';
+import { Text, Button, Box } from '@chakra-ui/react';
+import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
+import '../App.css';
 
 export default function Home() {
   const navigate = useNavigate();
@@ -11,7 +11,10 @@ export default function Home() {
     <div>
       <Navbar />
       <div className="body">
-        <h1 className="pageTitle">Home</h1>
+        <h1 className="pageTitle">Hi, I'm Maggie.</h1>
+        <div className="pagePara">
+          <p1>I'm a web developer with real life project experiences.</p1>
+        </div>
       </div>
     </div>
   );
--- src/Containers/mySkills.js
+++ src/Containers/mySkills.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Text, Button, Box, Link } from "@chakra-ui/react";
-import { useNavigate } from "react-router";
-import "../App.css";
-import Navbar from "../Components/Navbar";
+import React, { useState, useEffect, useContext } from 'react';
+import { Text, Button, Box, Link } from '@chakra-ui/react';
+import { useNavigate } from 'react-router';
+import '../App.css';
+import Navbar from '../Components/Navbar';
 
 export default function MySkills() {
   const navigate = useNavigate();
@@ -10,7 +10,7 @@ export default function MySkills() {
     <div>
       <Navbar />
       <div className="body">
-        <h1 className="pageTitle">My Skills</h1>
+        <h1 className="pageTitle">What I can do</h1>
       </div>
     </div>
   );
