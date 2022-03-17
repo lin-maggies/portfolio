7022a267011c60832b6c9e243b9e2cb9b4071d6c
maggie <lin.maggies@gmail.com>
2022-03-17 13:54:27 +1300

    add some logo, make contact form into component, add content to landing page

--- src/Containers/contactMe.js
+++ src/Containers/contactMe.js
@@ -1,77 +1,17 @@
-import React, { useState } from 'react';
-import { Text, Button, Box, Link } from '@chakra-ui/react';
-import '../App.css';
-import Navbar from '../Components/Navbar';
+import React from "react";
+import "../App.css";
+import Navbar from "../Components/Navbar";
+import ContactForm from "../Components/ContactForm";
 
 export default function ContactMe() {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const endpoint =
-      'https://1gmprjswn1.execute-api.ap-southeast-2.amazonaws.com/default/sendContactEmail';
-
-    const body = JSON.stringify({
-      senderName: name,
-      senderEmail: email,
-      message: message,
-    });
-
-    const requestOptions = {
-      method: 'POST',
-      body,
-    };
-
-    fetch(endpoint, requestOptions)
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
   return (
     <div>
       <Navbar />
       <div className="body">
         <h1 className="pageTitle">Send me a message</h1>
-        <form onSubmit={handleSubmit} className="contact-form">
-          <label>Email address</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-          />
-          <label>Name</label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter name"
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            required
-          />
-          <label>Message</label>
-          <input
-            type="text"
-            name="message"
-            placeholder="Enter message"
-            onChange={(e) => setMessage(e.target.value)}
-            value={message}
-            required
-          />
-          <button type="submit">Submit</button>
-        </form>
+        <ContactForm />
         <p1>
-          Or send me an email at {''}
+          Or send me an email at {""}
           <a href="mailto:lin.maggies@gmail.com">lin.maggies@gmail.com</a>
         </p1>
       </div>
--- src/Containers/mySkills.js
+++ src/Containers/mySkills.js
@@ -1,8 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { Text, Button, Box, Link } from '@chakra-ui/react';
-import { useNavigate } from 'react-router';
-import '../App.css';
-import Navbar from '../Components/Navbar';
+import React, { useState, useEffect, useContext } from "react";
+import { Text, Button, Box, Link } from "@chakra-ui/react";
+import { useNavigate } from "react-router";
+import "../App.css";
+import Navbar from "../Components/Navbar";
+import awsLogo from "../assets/images/aws.svg";
+import cssLogo from "../assets/images/css.svg";
+import gitLogo from "../assets/images/git.svg";
+import htmlLogo from "../assets/images/html.svg";
+import jsLogo from "../assets/images/javascript.svg";
+import reactLogo from "../assets/images/react.svg";
 
 export default function MySkills() {
   const navigate = useNavigate();
@@ -11,6 +17,15 @@ export default function MySkills() {
       <Navbar />
       <div className="body">
         <h1 className="pageTitle">What I can do</h1>
+        <div className="logoContainer">
+          <img src={htmlLogo} alt="HTML logo" className="skillLogo" />
+          <img src={jsLogo} alt="Javascript logo" className="skillLogo" />
+          <img src={cssLogo} alt="CSS logo" className="skillLogo" />
+          <img src={awsLogo} alt="AWS logo" className="skillLogo" />
+          <img src={gitLogo} alt="Git logo" className="skillLogo" />
+
+          <img src={reactLogo} alt="React logo" className="skillLogo" />
+        </div>
       </div>
     </div>
   );
